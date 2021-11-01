dd909782fae6383918436267aef21a0cdc0a2fdb
nidhi-rathore <nidhirathore7022@gmail.com>
2021-11-01 21:58:57 +0530

    first react app

---
+++ src/components/Node.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Node() {
+    return (<div class="note">
+    <h1>This is a Title.</h1>
+    <p>This is a Paragraph.</p>
+    </div>
+    );
+}
+
+export default Node;
